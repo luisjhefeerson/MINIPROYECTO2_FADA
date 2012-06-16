1557f6ba5872e24b49da15ca12f2519da1c3d88a
Gustavo Adolfo Rodriguez <gustalibreros@gmail.com>
2012-06-16 15:54:11 -0500

    Update

--- src/logica/TriangulacionMinimalDinamica.java
+++ src/logica/TriangulacionMinimalDinamica.java
@@ -11,8 +11,70 @@ import java.awt.Point;
  * @author gadolforl
  */
 public class TriangulacionMinimalDinamica {
-    
-    Point[] Vertices;
-    
-    
+
+    private Point[] vertices;
+    private int nVertices;
+    private Celda[][] matrizCostos;
+
+    public TriangulacionMinimalDinamica() {
+    }
+
+    public TriangulacionMinimalDinamica(Point[] vertices, int nVertices) {
+        this.vertices = vertices;
+        this.nVertices = nVertices;
+    }
+
+    /**
+     * Get the value of matrizCostos
+     *
+     * @return the value of matrizCostos
+     */
+    public Celda[][] getMatrizCostos() {
+        return matrizCostos;
+    }
+
+    /**
+     * Set the value of matrizCostos
+     *
+     * @param matrizCostos new value of matrizCostos
+     */
+    public void setMatrizCostos(Celda[][] matrizCostos) {
+        this.matrizCostos = matrizCostos;
+    }
+
+    /**
+     * Get the value of nVertices
+     *
+     * @return the value of nVertices
+     */
+    public int getNVertices() {
+        return nVertices;
+    }
+
+    /**
+     * Set the value of nVertices
+     *
+     * @param nVertices new value of nVertices
+     */
+    public void setNVertices(int nVertices) {
+        this.nVertices = nVertices;
+    }
+
+    /**
+     * Get the value of vertices
+     *
+     * @return the value of vertices
+     */
+    public Point[] getVertices() {
+        return vertices;
+    }
+
+    /**
+     * Set the value of vertices
+     *
+     * @param vertices new value of vertices
+     */
+    public void setVertices(Point[] vertices) {
+        this.vertices = vertices;
+    }
 }
