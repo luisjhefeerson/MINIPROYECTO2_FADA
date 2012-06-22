01f7f3db9608af12db752686a6ce6f9825c1dbc0
Gustavo Adolfo Rodriguez <gustalibreros@gmail.com>
2012-06-22 17:12:34 -0500

    Delete Main

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -32,6 +32,8 @@ import java.io.File;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import logica.Poligono;
+import logica.TrianguladorMinimalDinamico;
+import logica.Vertice;
 
 /**
  * Class description
@@ -305,6 +307,21 @@ public class MainFrame extends javax.swing.JFrame {
      */
     public static void main(String args[]) {
 
+//        Vertice[] vertices = new Vertice[7];
+//
+//        vertices[0] = new Vertice(0, 0, 10);
+//        vertices[1] = new Vertice(1, 0, 20);
+//        vertices[2] = new Vertice(2, 8, 26);
+//        vertices[3] = new Vertice(3, 15, 26);
+//        vertices[4] = new Vertice(4, 27, 21);
+//        vertices[5] = new Vertice(5, 22, 12);
+//        vertices[6] = new Vertice(6, 10, 0);
+//
+//        Poligono                    poligono     = new Poligono(vertices, 7);
+//        TrianguladorMinimalDinamico triangulador = new TrianguladorMinimalDinamico(poligono);
+//
+//        System.out.print("Costo de triangulacion Minimal: " + triangulador.calcularTriangulacion(0, 7));
+        
         // <editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
 
         /*
