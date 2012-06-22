fff058cb0c5b04fc3f2aa9b933278e879ec450c0
Gustavo Adolfo Rodriguez <gustalibreros@gmail.com>
2012-06-22 17:14:52 -0500

    Encabezado

--- src/gui/MainFrame.java
+++ src/gui/MainFrame.java
@@ -1,30 +1,33 @@
-//**********************************************************
+//*************************************************************
 // FUNDAMENTOS DE ANÁLISIS Y DISEÑO DE ALGORITMOS
-// MINIPROYECTO 1: SOLUCIÓN PARA UN JUEGO DE LÓGICA: AKARI
+// MINIPROYECTO 2 - FADA: PROGRAMACIÓN DINÁMICA Y VORAZ:
+//                        TRIANGULACIÓN DE POLÍGONOS
+//
+// WEB PAGE: http://gustalibreros.github.com/MINIPROYECTO2_FADA
+//
+// FECHA INICIACION: JUNIO DE 2012
 //
 // ARCHIVO: MainFrame.java
 //
-// FECHA:  01.06.12
+// FECHA:  22.06.12
 //
 // AUTORES:
 //     Marx Arturo Arias - 0840247-3743
 //     marxarturoariasescobar@gmail.com
 //
-//     Luis Jeferson Gallardo - 
+//     Luis Jeferson Gallardo - 0843805-2711
 //     luisjhefeerson@gmail.com
 //
 //     Fidel Herney Palacios - 0832691-3743
 //     herney400@gmail.com
 //
-//     James Andrés Payan - 0957150-2711
-//     jamespayan@gmail.com
-//
-//     Gustavo Adolfo Rodriguez - 0932979-3743
+//     Gustavo Adolfo Rodriguez - 0932979-3743    
 //     gustalibreros@gmail.com
 //
 // ESCUELA DE INGENIERIA DE SISTEMAS Y COMPUTACION
 // UNIVERSIDAD DEL VALLE
-//**********************************************************
+//*************************************************************
+
 package gui;
 
 
@@ -32,8 +35,6 @@ import java.io.File;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import logica.Poligono;
-import logica.TrianguladorMinimalDinamico;
-import logica.Vertice;
 
 /**
  * Class description
